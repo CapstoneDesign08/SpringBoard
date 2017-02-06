2f266ddf22b053d7a1f714bd5b224cc3201e0df4
JungChanHyeong <chanh327@gmail.com>
2017-02-06 14:07:02 +0900

    error message test

--- src/main/java/com/board/model/Post.java
+++ src/main/java/com/board/model/Post.java
@@ -15,4 +15,52 @@ public class Post {
     private String content;     // 내용
     private String date;          // 날짜
     private int hit;            // 조회
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getHit() {
+        return hit;
+    }
+
+    public void setHit(int hit) {
+        this.hit = hit;
+    }
 }
